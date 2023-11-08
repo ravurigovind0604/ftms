0e4f1540e5d4345fd227d3e640ff59a04dcf6af9
Ravuri govind <67058717+ravurigovind0604@users.noreply.github.com>
2023-11-08 11:09:11 -0600

    Add files via upload

---
+++ flightTicketManagementSystem/src/main/java/com/example/flightTicketManagementSystem/FlightTicketManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightTicketManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketManagementSystemApplication.class, args);
+	}
+
+}
---
+++ flightTicketManagementSystem/src/test/java/com/example/flightTicketManagementSystem/FlightTicketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightTicketManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
